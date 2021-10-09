1b7d37f47bc0f9266faa49d414b69eeed9ed13d6
John Newman <john.michael.newman@gmail.com>
2021-10-09 20:10:27 +0000

    don't ignore kondo config, update release

--- build.clj
+++ build.clj
@@ -4,7 +4,7 @@
             [org.corfield.build :as bb]))
 
 (def lib 'net.clojars.john/injest)
-(def version "0.1.0-alpha.18")
+(def version "0.1.0-alpha.19")
 #_ ; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
 (def url "https://github.com/johnmn3/injest")
